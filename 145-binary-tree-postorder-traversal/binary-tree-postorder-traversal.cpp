17e5a1ce439f9fd1e13715a8f2b954d125da340b
Supreme Dey <supremedey590@gmail.com>
2026-05-26 11:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (43.38%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+
+            ans.push_back(node->val);
+
+            // Push left first, then right
+            if (node->left) st.push(node->left);
+            if (node->right) st.push(node->right);
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;   // Important
+    }
+};
