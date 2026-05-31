8830419e395a39bf299f4140f91e57e132549449
Supreme Dey <supremedey590@gmail.com>
2026-05-31 16:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (68.63%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null || head.next==null) return null;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(fast!=slow) return null;
+        ListNode temp=head;
+        while(temp!=slow){
+            temp=temp.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+}
