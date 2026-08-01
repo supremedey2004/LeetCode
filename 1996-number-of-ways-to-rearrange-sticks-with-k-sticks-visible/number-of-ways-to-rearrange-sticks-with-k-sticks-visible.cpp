818754e5db81c09bba6c96d6cd90661006a55946
Supreme Dey <supremedey590@gmail.com>
2026-08-01 20:28:32 +0530

    Time: 67 ms (69.93%) | Memory: 122.3 MB (44.16%) - LeetSync

---
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rearrangeSticks(int n, int k) {
+        const int MOD = 1000000007;
+
+        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= min(i, k); j++) {
+                dp[i][j] = (dp[i - 1][j - 1] +
+                           (long long)(i - 1) * dp[i - 1][j]) % MOD;
+            }
+        }
+
+        return dp[n][k];
+    }
+};
